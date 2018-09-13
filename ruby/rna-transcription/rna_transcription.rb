9c7eb0fd2d12874126a985f1524e64f0ccf86736
Marina Vega <marina.vega@cabify.com>
2018-09-13 18:30:42 +0200

    Add RNA kata

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,3 +1,35 @@
-class RnaTranscription
-    
+module BookKeeping
+  VERSION = 4
+end
+
+class Complement
+  DNA_TO_RNA = {
+    'G': 'C',
+    'C': 'G',
+    'T': 'A',
+    'A': 'U'
+  }.freeze
+  
+  class << self
+    def of_dna(dna)
+      dna_to_rna(dna)
+    end
+  
+    def dna_to_rna(dna)
+      rna = ''
+      dna.each_char do |nucleotid|
+        return '' if invalid_nucleotid?(nucleotid)
+        rna << complementary_nucleotid(nucleotid)
+      end
+      rna
+    end
+  
+    def complementary_nucleotid(nucleotid)
+      DNA_TO_RNA[nucleotid.to_sym]
+    end
+  
+    def invalid_nucleotid?(nucleotid)
+      true if complementary_nucleotid(nucleotid).nil?
+    end
+  end
 end
