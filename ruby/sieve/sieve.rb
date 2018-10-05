2cf69534de41875819d6e149e0b3083bc93dc36e
Marina Vega <marina.vega@cabify.com>
2018-10-05 11:45:53 +0200

    Add sieve kata solved

---
+++ ruby/sieve/sieve.rb
@@ -0,0 +1,23 @@
+class Sieve
+  attr_reader :primes
+  def initialize(number)
+    @primes = generate_primes(number)
+    
+  end
+
+  private
+
+  def generate_primes(number)
+    number_list = (2..number).to_a
+    primes = []
+
+    while number_list.any?
+      prime = number_list.shift
+      primes << prime
+
+      number_list.reject! { |candidate| (candidate % prime).zero? }
+    end
+
+    primes
+  end
+end
